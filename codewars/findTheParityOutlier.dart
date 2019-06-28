6e1a9f802353e1460ef399cbe25d01c5029dac83
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-06-28 12:36:59 -0500

    Codewars (Find The Parity Outlier )

---
+++ codewars/findTheParityOutlier.dart
@@ -0,0 +1,32 @@
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+
+// Examples
+// [2, 4, 0, 100, 4, 11, 2602, 36]
+// Should return: 11 (the only odd number)
+
+// [160, 3, 1719, 19, 11, 13, -21]
+// Should return: 160 (the only even number)
+
+void main() {
+  int find(List integers) {
+    var even = [];
+    var odd = [];
+
+    for (var num in integers) {
+      if (num % 2 == 0) {
+        even.add(num);
+      } else {
+        odd.add(num);
+      }
+    }
+    if (even.length == 1) {
+      print(even[0]);
+      // return even[0];
+    } else {
+      print(odd[0]);
+      // return odd[0];
+    }
+  }
+
+  find([2, 4, 0, 100, 4, 11, 2602, 36]);
+}
